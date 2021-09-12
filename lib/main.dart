b079fc63f5c9c925ef94c40be068ea07b1ce8a74
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-09-12 23:29:20 +0600

    code complete

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,61 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: (SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: (AssetImage('images/man.png')),
+              ),
+              Text(
+                'Nafiz Zaman',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontFamily: ('Pacifico'),
+                    fontWeight: FontWeight.normal),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: ('Roboto'),
+                  letterSpacing: 4,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.phone_android),
+                      title: Text(
+                        '+880 1730261340',
+                        style: TextStyle(fontFamily: ('Roboto')),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.email_outlined),
+                      title: Text(
+                        'nafizzamanarnob17@gmail.com',
+                        style: TextStyle(fontFamily: ('Roboto')),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )),
       ),
     ),
   );
